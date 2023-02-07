8700b8f6d44939f08f8b22556ba3b2516feffda5
ToxPuro <purotouko2@gmail.com>
2023-02-07 12:25:51 +0200

    parser works

--- parse.py
+++ parse.py
@@ -1,8 +1,11 @@
 
+from asyncore import write
 import math
 import re
 import os
 import csv
+
+from pexpect import split_command_line
 
 
 
@@ -23,6 +26,8 @@ class Parser:
                 search = re.search("([^\s]+\.f90)", line)
                 if search is not None:
                     self.used_files.append(f"./pencil-code/src/{search.group(1)}")
+        ignored_files = ["./pencil-code/src/boundcond.f90","./pencil-code/src/nompicomm.f90","./pencil-code/src/mpicomm.f90","./pencil-code/src/mpicomm_double.f90"]
+        self.used_files = list(filter(lambda x: x not in ignored_files,self.used_files))
 
     def find_module_files(self, module_name, rootdir):
         res = [file for file in self.used_files if self.contains_module(self.get_lines(file), module_name)]
@@ -67,13 +72,16 @@ class Parser:
                     if line[0] != "!":
                         line = self.parse_line(line)
                     if len(line)>0:
+                        #take care the case that the line continues after end of the line
                         if line[-1] == "&":
                             while line[-1] == "&":
                                 index += 1
                                 next_line = read_lines[index].strip()
                                 if next_line[0] != "!" and len(next_line)>0:
                                     line = (line[:-1] + " " + self.parse_line(next_line)).strip()
-                        lines.append((line, index))
+                        # split multilines i.e. fsdfaf;fasdfsdf; into their own lines for easier parsing
+                        for line in self.split_line(line):
+                            lines.append((line, index))
                 index += 1
             self.lines[filepath] = lines
         res = [x for x in self.lines[filepath] if x[1] >= start and x[1] <= end]
@@ -87,9 +95,9 @@ class Parser:
                 return True
         return False
 
-    def contains_subroutine(self, lines, subroutine_name):
+    def contains_subroutine(self, lines, function_name):
         for (line, count) in lines:
-            if line.split("(")[0].strip() == f"subroutine {subroutine_name}":
+            if re.search(f"\s?subroutine {function_name}[\(\s]",line) or re.search(f"\s?function {function_name}[\(\s]",line) or re.search(f"interface {function_name}\s",line):
                 return True
         return False
 
@@ -157,7 +165,7 @@ class Parser:
                 num_of_double_quotes += 1
             elif elem == "=" and line[index-1] not in "<>=!" and line[index+1] not in "<>=!" and num_of_single_quotes%2==0 and num_of_double_quotes%2==0 and num_of_left_brackets == num_of_right_brackets:
                 write = self.parse_write_variable(line[start_index:index])
-                res.append((write, count, line))
+                res.append({"variable": write, "line_num": count, "line": line, "is_static": write in self.static_variables})
                 start_index = index
             elif elem == "(":
                 num_of_left_brackets += 1
@@ -213,9 +221,8 @@ class Parser:
         subroutine_line_start = 0
         subroutine_line_end = 0
         for (line, count) in self.get_lines(filename):
-            if line.split("(")[0].strip() == f"subroutine {function_name}":
+            if  re.search(f"\s?subroutine {function_name}[\(\s]",line) or re.search(f"\s?function {function_name}[\(\s]",line):
                 subroutine_line_start = count
-
             # line.strip() == f"endsubroutine {function_name}" or line.strip() == f"end subroutine {function_name}"    
             if  (re.match(f"end.+{function_name}",line) or (line == "end" or line == "endsubroutine")) and subroutine_line_start>0:
                 subroutine_line_end = count
@@ -389,41 +396,50 @@ class Parser:
         subroutine_line_start, subroutine_line_end = self.get_subroutine_line_start_and_end(filename, subroutine_name)
         return self.get_lines(filename, subroutine_line_start, subroutine_line_end)
 
-    def parse_subroutine_all_files(self, subroutine_name, rootdir, call_trace, layer_depth=math.inf):
+    def get_static_parameters(self,line,parameter_list):
+        check_subroutine= re.search("\s?subroutine.+\((.+)\)",line)
+        if check_subroutine:
+            parameters = [parameter.split("=")[-1].split("(")[0].strip() for parameter in check_subroutine.group(1).split(",")]
+        else:
+            parameters = [parameter.split("=")[-1].split("(")[0].strip() for parameter in re.search(".?function.+\((.+)\)",line).group(1).split(",")]
+        return list(zip(parameters,parameter_list))
+        
+    def get_static_passed_parameters(self,parameters,local_variables):
+        return list(zip([parameter.split("=")[-1].split("(")[0] for parameter in parameters],list(map(lambda x: x.split("=")[-1].split("(")[0] in self.static_variables and x.split("=")[-1].split("(")[0] not in local_variables,[parameter.split("=")[-1].split("(")[0] for parameter in parameters]))))
+
+    def get_interfaced_functions(self,file_path,subroutine_name):
+        res = []
+        lines = self.get_lines(file_path)
+        for i,(line, count) in enumerate(lines):
+            if line.split(" ")[0] == "interface" and len(line.split(" "))>1 and line.split(" ")[1].strip() == subroutine_name:
+               
+                cur_index = i+1
+                cur_line = lines[cur_index][0]
+                while not re.match("endinterface",cur_line):
+                    cur_index += 1
+                    res.append(cur_line.split("module procedure ")[1].strip())
+                    cur_line = lines[cur_index][0]
+                break
+        if len(res) == 0:
+            return [subroutine_name]
+        return res
+
+        
+
+    def parse_subroutine_all_files(self, subroutine_name, rootdir, call_trace, layer_depth=math.inf, parameter_list=[], only_static=True):
         self.subroutine_order += 1
         static_writes = []
         if subroutine_name not in self.parsed_subroutines:
             self.parsed_subroutines.append(subroutine_name)
             file_paths = self.find_subroutine_files(subroutine_name, rootdir)
             for file_path in file_paths:
-                static_writes.extend(self.parse_subroutine_in_file(file_path, subroutine_name, layer_depth, call_trace))
+                interfaced_functions = self.get_interfaced_functions(file_path,subroutine_name)
+                for function in interfaced_functions:
+                    static_writes.extend(self.parse_subroutine_in_file(file_path, function, layer_depth, call_trace,parameter_list,only_static))
         return static_writes
 
 
-    def parse_subroutine_in_file(self, filename, subroutine_name, layer_depth=math.inf, call_trace=""):
 
-        if layer_depth < 0:
-            return []
-        subroutine_line_start, subroutine_line_end = self.get_subroutine_line_start_and_end(filename, subroutine_name)
-        lines = self.get_lines(filename, subroutine_line_start, subroutine_line_end)
-        modules = self.get_used_modules(lines)
-        self.parse_file_for_static_variables(filename)
-        for module in self.get_subroutine_modules(filename, subroutine_name):
-            self.parse_module(module, "./pencil-code/src")
-
-        local_variables = {}
-        local_variables = self.get_variables(lines, local_variables,filename)
-        function_calls = self.get_function_calls(lines, local_variables)
-        
-        writes = self.get_writes(lines)
-        if self.subroutine_order == 0:
-            call_trace = subroutine_name
-        else:
-            call_trace = f"{call_trace} -> {subroutine_name}"
-        static_writes = [{"variable": x[0],"line_num": x[1], "filename": filename, "call_trace":call_trace, "line": x[2]} for x in writes if (x[0] not in local_variables and x[0] in self.static_variables)]
-        for function_call in function_calls:
-            static_writes.extend(self.parse_subroutine_all_files(function_call["function_name"], "./pencil-code/src", call_trace, layer_depth-1))
-        return static_writes
 
     def generate_save_array_store(self,store_variable):
         res = f"{store_variable}_generated_array(imn"
@@ -488,39 +504,76 @@ class Parser:
         with open("writes.csv", mode="r") as infile:
             return [dict for dict in csv.DictReader(infile)]
 
-
-    def make_variables_threadprivate(self,variables):
-        for file in self.used_files:
+    def write_to_files(self,files,threadprivate_variables,critical_variables):
+        for file in files:
             already_threadprivate = self.add_threadprivate_declarations_in_file(file)
             res_contents = []
             contents = self.get_lines(file,include_comments=True)
             in_module_declaration = True
             in_type = False
-            for(line,count) in contents:
+            in_where = False
+            last_where = 0
+            need_to_add_critical_at_the_end_of_where = False
+            for i, (line,count) in enumerate(contents):
                 res_contents.append(line)
                 if line.split(" ")[0] == "type":
                     in_type = True
                 if line.split(" ")[0] == "endtype":
                     in_type = False
-                if in_module_declaration and not in_type:
+                if in_module_declaration and not in_type and line[0] != "!":
                     if re.match("function\s+.+\(.+\)",line) or re.match("subroutine\s+.+\(.+\)",line) or line.strip() == "contains":
                         in_module_declaration = False
                     parts = line.split("::")
                     if len(parts) > 1:
                         line_variables = parts[1].strip()
                         variable_names = self.get_variable_names_from_line(line_variables)
-                        res_contents.extend([f"!$omp threadprivate({variable})" for variable in variable_names if variable in variables and variable not in already_threadprivate])
-            
+                        res_contents.extend([f"!$omp threadprivate({variable})" for variable in variable_names if variable in threadprivate_variables and variable not in already_threadprivate])
+                if line[0] != "!":
+                    #!$omp criticals
+                    writes = self.get_writes_from_line(line,count)
+
+                    where_end = line.split(" ")[0].strip() == "endwhere" or line.split(" ")[0].strip() == "end where"
+                    if in_where and where_end:
+                        in_where = False
+                        if need_to_add_critical_at_the_end_of_where:
+                            need_to_add_critical_at_the_end_of_where = False
+                            res_contents.append("!$omp end critical\n")
+                    #handle where writes
+                    if  line.split("(")[0].strip() == "where":
+                        last_where = len(res_contents)-1
+                        #let's see if there is a corresponding endwhere
+                        current_index = i+1
+                        no_end_where= True
+                        iter_line = contents[current_index][0]
+                        while no_end_where and not iter_line.split("(")[0].strip() == "where" and current_index<len(contents):
+                            
+                            
+                            no_end_where = not(iter_line.split(" ")[0].strip() == "endwhere" or iter_line.split(" ")[0].strip() == "end where")
+                            current_index += 1
+                            if current_index<len(contents):
+                                iter_line = contents[current_index][0]
+                        in_where = not no_end_where
+                                
+                    for variable in critical_variables:
+                        if variable in [write[0] for write in writes]:
+                            if in_where:
+                                if res_contents[last_where].split("\n")[0] != "!$omp critical":
+                                    need_to_add_critical_at_the_end_of_where = True
+                                    res_contents[last_where] = f"!$omp critical\n{res_contents[last_where]}"
+                            else:
+                                last_line = res_contents[-1]
+                                res_contents[-1] = "!$omp critical\n"
+                                res_contents.append(last_line)
+                                res_contents.append("!$omp end critical\n")
             with open(f"./out/{file}","w") as f:
                 f.write("\n".join(res_contents))
+
                     
     def add_atomic_declarations(self,variables):
         files = self.used_files
         for file in files:
             res_contents = []
             contents = self.get_lines(file,include_comments=True)
-            in_module_declaration = True
-            in_type = False
             for i,(line,count) in enumerate(contents):
                 res_contents.append(line)
                 if line[0] != "!":
@@ -624,45 +677,84 @@ class Parser:
             contents = "".join(res_contents)
             f.write(contents)
 
+    def parse_subroutine_in_file(self, filename, subroutine_name, layer_depth=math.inf, call_trace="", parameter_list=[], only_static=True):
+        if layer_depth < 0:
+            return []
+        subroutine_line_start, subroutine_line_end = self.get_subroutine_line_start_and_end(filename, subroutine_name)
+        lines = self.get_lines(filename, subroutine_line_start+1, subroutine_line_end)
+        modules = self.get_used_modules(lines)
+        self.parse_file_for_static_variables(filename)
+        for module in self.get_subroutine_modules(filename, subroutine_name):
+            self.parse_module(module, "./pencil-code/src")
+        local_variables = {parameter:v for parameter,v in self.get_variables(lines, {},filename).items() }
+        parameters = self.get_static_parameters(self.get_lines(filename,start=subroutine_line_start,end=subroutine_line_start)[0][0], parameter_list)
+        if self.subroutine_order == 0:
+            call_trace = subroutine_name
+        else:
+            call_trace = f"{call_trace} -> {subroutine_name}"
+        writes = self.get_writes(lines)
+        
+        for write in writes:
+            if write["variable"] == "costh(m)":
+                print("Not parameter",write["line"])
+            write["is_static"] = write["variable"] in self.static_variables and write["variable"] not in local_variables
+            for (parameter,(passed_parameter,is_static)) in parameters:
+                if write["variable"] == parameter:
+                    write["variable"] = passed_parameter
+                    write["is_static"] = is_static
+                    if write["variable"] == "costh(m)":
+                        print("Parameter",write["line"])
+
+        if only_static:
+            static_writes = [{"variable": write["variable"],"line_num": write["line_num"], "filename": filename, "call_trace":call_trace, "line": write["line"]} for write in writes if write["is_static"]]
+        else:
+            static_writes = [{"variable": x[0],"line_num": x[1], "filename": filename, "call_trace":call_trace, "line": x[2]} for x in self.get_writes(lines) if x[0]]
+        for function_call in self.get_function_calls(lines, local_variables):
+            static_writes.extend(self.parse_subroutine_all_files(function_call["function_name"], "./pencil-code/src", call_trace, layer_depth-1, self.get_static_passed_parameters(function_call["parameters"],local_variables),only_static))
+        return static_writes
+
+    def split_line(self, line):
+        if line[0] == "!":
+            return [line]
+        lines = []
+        split_indexes = []
+        num_of_single_quotes = 0
+        num_of_double_quotes = 0
+        num_of_left_brackets = 0
+        num_of_right_brackets = 0
+        for iter_index in range(len(line)):
+            if line[iter_index] == "'":
+                num_of_single_quotes += 1
+            if line[iter_index] == '"':
+                num_of_double_quotes += 1
+            if line[iter_index] == "(":
+                num_of_left_brackets += 1
+            if line[iter_index] == ")":
+                num_of_right_brackets += 1
+            if line[iter_index] == ";" and num_of_single_quotes%2 == 0 and num_of_double_quotes%2 == 0 and num_of_left_brackets == num_of_right_brackets:
+                split_indexes.append(iter_index)
+        start_index = 0
+        for split_index in split_indexes:
+            lines.append(line[start_index:split_index])
+            start_index=split_index+1
+        lines.append(line[start_index:])
+        return filter(lambda x: x != "",lines)
+
 
 def main():
     filename = "./pencil-code/src/equ.f90"
     subroutime_name = "rhs_cpu"
     parser = Parser()
-    #parser.load_static_variables("./pencil-code/src/cdata.f90")
-   # static_writes = parser.parse_subroutine_in_file("./pencil-code/src/entropy.f90","calc_heat_cool",0)
-   # parser.save_static_writes(static_writes)
-    #for (line,count) in lines:
-    #    print(line)
-    #parser.parse_file_for_static_variables("./pencil-code/src/hydro.f90")
-
-   # lines = parser.get_lines("./pencil-code/src/geometrical_types.f90",include_comments=True)
-   # for (line,count) in lines:
-   #     print(line)
-
-    static_writes = parser.parse_subroutine_in_file(filename, subroutime_name)
-    parser.save_static_variables()
-    parser.save_static_writes(static_writes)
-    #static_writes = parser.read_static_writes()
-    variables = list(set([x["variable"] for x in static_writes]))
-    variables = list(set(variables) - set(["imn","fname","fnamer","fnamex","fnamey","fnamez","fnamexy","fnamexz","fnamerz","itype_name","dt1_max_array","dt1_max","f","df","p"]))
-    for variable in variables:
-        print(variable)
-    parser.make_variables_threadprivate(variables)
+    writes = parser.parse_subroutine_in_file(filename, subroutime_name)
+    parser.save_static_writes(writes)
+    variables = list(set([x["variable"] for x in writes]))
+    variables = list(set(variables) - set(["f","df","p"]))
     print(f"Num of static variables written with full depth {len(variables)}")
-    parser.add_atomic_declarations(["fname","fnamer","fnamex","fnamey","fnamez","fnamexy","fnamexz","fnamerz","itype_name","dt1_max"])
+    parser.write_to_files(parser.used_files,variables,[])
+   
 
-    #filename = "./pencil-code/src/entropy.f90"
-    #subroutine_name = "calc_heat_cool"
-    #subroutine_line_start, subroutine_line_end = parser.get_subroutine_line_start_and_end(filename, subroutine_name)
-    #lines = parser.get_lines(filename, subroutine_line_start, subroutine_line_end)
-   # parser.parse_file_for_static_variables(filename)
-   # for module in parser.get_subroutine_modules(filename, subroutine_name):
-   #     parser.parse_module(module, "./pencil-code/src")
-
-    #local_variables = {}
-    #local_variables = parser.get_variables(lines, local_variables,filename)
-    #for variable in local_variables:
-    #    print(variable)
 if __name__ == "__main__":
     main()
+
+
+
